771792858eb35859ebf9c4e1a722723bd382d493
shaggy245 <shaggy245@users.noreply.github.com>
2020-09-01 22:32:42 -0500

    add input sanitation
    move erase to own function since it can be called when `erase` arg or
    if an empty string is passed into `store`

--- main.go
+++ main.go
@@ -6,13 +6,20 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mitchellh/go-homedir"
 )
 
-func updateTokenHelper(tokens map[string]string, tokenPath string) error {
+func updateTokens(tokens map[string]string, tokenPath string) error {
 	tokenJ, _ := json.Marshal(tokens)
 	err := ioutil.WriteFile(tokenPath, tokenJ, 0644)
+	return err
+}
+
+func eraseToken(tokens map[string]string, vaultAddr string, tokenPath string) error {
+	delete(tokens, vaultAddr)
+	err := updateTokens(tokens, tokenPath)
 	return err
 }
 
@@ -33,33 +40,42 @@ func main() {
 		json.Unmarshal(data, &tokenData)
 	}
 
-	fmt.Println(tokenData)
-
-	vault_addr := os.Getenv("VAULT_ADDR")
-	if vault_addr == "" {
+	rawVaultAddr := os.Getenv("VAULT_ADDR")
+	vaultAddr := strings.ToLower(strings.TrimSpace(rawVaultAddr))
+	vaultAddr = strings.TrimSuffix(vaultAddr, "/")
+	vaultAddr = strings.TrimPrefix(vaultAddr, "http://")
+	vaultAddr = strings.TrimPrefix(vaultAddr, "https://")
+	if vaultAddr == "" {
 		log.Fatal("Environment variable $VAULT_ADDR must be set")
 	}
 
 	switch os.Args[1] {
 	case "get":
 		// get
-		token := tokenData[vault_addr]
+		token := tokenData[vaultAddr]
 		fmt.Print(token)
 	case "erase":
 		// erase
-		delete(tokenData, vault_addr)
-		err := updateTokenHelper(tokenData, tokenPath)
+		err := eraseToken(tokenData, vaultAddr, tokenPath)
 		if err != nil {
 			log.Fatal(err)
 		}
 	case "store":
 		// store
-		var input string
-		fmt.Scanln(&input)
-		tokenData[vault_addr] = input
-		err := updateTokenHelper(tokenData, tokenPath)
-		if err != nil {
-			log.Fatal(err)
+		var rawInput string
+		fmt.Scanln(&rawInput)
+		input := strings.TrimSpace(rawInput)
+		if input == "" {
+			err := eraseToken(tokenData, vaultAddr, tokenPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			tokenData[vaultAddr] = input
+			err := updateTokens(tokenData, tokenPath)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	default:
 		log.Fatal("Requires one arg of 'get', 'erase', or 'store'")
